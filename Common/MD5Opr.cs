c4767606a364b4d12027b1e0883771c652459b9c
dingye621 <dingye621@126.com>
2015-08-08 14:43:09 +0800

    add md5function

---
+++ Common/MD5Opr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common
+{
+    public class MD5Opr
+    {
+        public static string getMd5Hash(string args)
+        {
+            MD5 md = MD5.Create();
+
+            byte[] data = md.ComputeHash(Encoding.Default.GetBytes(args));
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in data)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
